206dbdab81d231ecdbde21347a502946eb98e8f8
dannybtaa <danielabetancur.arr@gmail.com>
2024-01-29 13:46:54 -0500

    probando

---
+++ hello.py
@@ -0,0 +1,52 @@
+print("helo")
+
+
+'''
+tenia un error asi que busque entre el curso de github se soluciona asi:
+1.CREAR LLAVES:
+
+tener en cuenta que LAS LLAVES SON PARA CADA PC, 
+no es buena idea compartir las llaves 
+
+para ver que email tengo vinculado
+git config -l
+
+mi email de github
+danielabetancur.arr@gmail.com
+
+cambiar correo
+git config --global user.email "correo@gmail.com"
+
+conectamos la llave
+ssh-keygen -t rsa -b 4096 -C "correo@gmail.com"
+
+crea una contraseña si quieres ahi 
+
+mirar si el servido de ssh funciona
+eval $(ssh-agent -s)
+debe aparecer algo asi
+Agent pid  ####
+
+agregar la llave
+ssh-add ~/.ssh/id_rsa
+
+en git hub vamos a:
+settings
+ssh and gpc
+
+en ssh hey pegar la cavle publica que esta en el archivo id_rsa.pub
+
+(como llegar a ese archivo, \\wsl$  vas a unbuntu y a tu home y alli esta el nombre de tu pc y por ahi debe estar la carpeta .ssh)
+
+eso debe funcionar
+
+
+
+1. git init
+
+2. pego link de github
+
+git remote add origin https://github.com/Dannybtaa/curso-python-pip.git
+
+3. verificamos con 
+git remote -v
